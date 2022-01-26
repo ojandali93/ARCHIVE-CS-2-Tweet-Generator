b5c16059994f30f8e6e06d2a8a2360d2a61cae1b
OmarJ <omarjandali93@gmail.com>
2022-01-25 21:36:01 -0800

    added the second histogram to analyzing frequency

--- histogram.py
+++ histogram.py
@@ -1,6 +1,6 @@
 import sys
 
-def histogram(word_text):
+def histogramDict(word_text):
   dictionary = {}
   for word in word_text:
     if word in dictionary.keys():
@@ -9,12 +9,35 @@ def histogram(word_text):
       dictionary[word] = 1
   return dictionary
 
-def unique_words(word_text):
-  dictionary = histogram(word_text)
+def histogramList(word_text):
+  word_list = []
+  for word in word_text:
+    for record in word_list:
+      if record[0] == word:
+        record[1] = record[1] + 1
+    else:
+      new_record = []
+      new_record.append(word)
+      new_record.append(1)
+      word_list.append(new_record)
+  print(word_list)
+  # sortWordList(word_list)
+
+# def sortWordList(word_list):
+#   histogram_list = []
+#   for record in word_list:
+#     if len(histogram_list) == 0:
+#       histogram_list.append(record)
+#     else:
+#       for check_record in histogram_list:
+#         if record[1]  check_record[1]
+
+def uniqueWords(word_text):
+  dictionary = histogramDict(word_text)
   print("There are [" + str(len(dictionary)) + "] unique words in the text.")
 
-def word_frequency(word, word_text):
-  dictionary = histogram(word_text)
+def wordFrequency(word, word_text):
+  dictionary = histogramDict(word_text)
   if dictionary[word]:
     if dictionary[word] > 1:
       print("There are [" + str(dictionary[word]) + "] instances of " + str(word) +" unique words in the text.")
@@ -31,6 +54,7 @@ if __name__ == '__main__':
   clean_text = blog_text.replace(',','').replace('.','').replace('?','').replace("'",'').replace('"','').replace('-','')
   word_text = clean_text.split(' ')
   f.close()
-  histogram(word_text)
-  unique_words(word_text)
-  word_frequency(word_check, word_text)
+  histogramDict(word_text)
+  histogramList(word_text)
+  uniqueWords(word_text)
+  wordFrequency(word_check, word_text)
